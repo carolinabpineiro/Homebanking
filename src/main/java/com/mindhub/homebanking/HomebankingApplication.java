6efe39dc90142eb0dba335232a5ebeb301ae9059
carolinabpineiro <carolinabpineiro@gmail.com>
2024-08-05 07:17:21 -0300

    Sprint 1 homebanking

---
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -0,0 +1,27 @@
+package com.mindhub.homebanking;
+
+import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HomebankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankingApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(ClientRepository repository) {
+		return (args) -> {
+			// save a couple of clients
+			repository.save(new Client("Mabel", "Morel", "melba@mindhub.com"));
+			repository.save(new Client("Thomas", "Maldonado", "totomaldopi@gmail.com"));
+
+		};
+	}
+
+}
